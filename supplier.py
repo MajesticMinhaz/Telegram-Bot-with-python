ba4399f2542ad51d56242db650ecb487fcc56f88
Md. Minhaz <mdm047767@gmail.com>
2022-11-06 21:26:45 +0600

    imported connect from sqlite3 package and connected with database file and also create a cursor.

--- supplier.py
+++ supplier.py
@@ -1,3 +1,4 @@
+from sqlite3 import connect
 from dotenv import dotenv_values
 
 
@@ -5,4 +6,11 @@ from dotenv import dotenv_values
 Get all values from .env file and store in config variable
 """
 config = dotenv_values(dotenv_path="./.env")
+
+
+"""
+Connect with SQLITE database file
+"""
+add_connection = connect(config.get("DATA_DB_PATH"))
+add_cursor = add_connection.cursor()
 
