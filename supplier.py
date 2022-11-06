76b5ab640bdc37ab9ab225de828294cbdf18d42f
Md. Minhaz <mdm047767@gmail.com>
2022-11-06 21:35:45 +0600

    imported sqlite commands from sqlite_commands file and created table for store data in a database

--- supplier.py
+++ supplier.py
@@ -1,5 +1,8 @@
 from sqlite3 import connect
 from dotenv import dotenv_values
+from sqlite_commands import create_client_information_table
+from sqlite_commands import create_group_information_table
+from sqlite_commands import create_user_information_table
 
 
 """
@@ -14,3 +17,10 @@ Connect with SQLITE database file
 add_connection = connect(config.get("DATA_DB_PATH"))
 add_cursor = add_connection.cursor()
 
+
+"""
+Creating basic tables in the database file
+"""
+add_cursor.execute(create_client_information_table)
+add_cursor.execute(create_group_information_table)
+add_cursor.execute(create_user_information_table)
